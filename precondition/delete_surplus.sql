d3ba5e6771304089f3d0994d5a332f1e48e74b1a
Song Jihun <sksmsvlxk@gmail.com>
2025-04-28 18:26:11 +0900

    Update: delete_surplus.sql 에서 annual_fuel_cost_for_fuel_type1 컬럼의 drop 쿼리를 주석처리.

--- precondition/delete_surplus.sql
+++ precondition/delete_surplus.sql
@@ -3,11 +3,19 @@
 create table all_vehicles_model_public_org like all_vehicles_model_public;          -- 테이블 구조 복사
 insert into all_vehicles_model_public_org select * from all_vehicles_model_public;  -- 테이블 데이터 복사
 
+;-- 위 쿼리는, dump 를 import 해서 작업하는 경우 all_vehicles_model_public 가 원본인 상태에서
+;-- 테이블을 all_vehicles_model_public_org 라는 이름으로 백업해놓기 위해 실행하는 쿼리임.
+
+;-- dump 를 import 한 직후 delete_surplus.sql 을 수행한 적 있다면,
+;-- 위 쿼리는 주석처리하고, 아래 쿼리를 수행해야 함.
+# create table all_vehicles_model_public like all_vehicles_model_public_org;          -- 테이블 구조 복사
+# insert into all_vehicles_model_public select * from all_vehicles_model_public_org;  -- 테이블 데이터 복사
+
 ;####################################################################
 ;-- 안쓰는 컬럼 삭제
 alter table all_vehicles_model_public drop column 2_door_passenger_volume;
 alter table all_vehicles_model_public drop column 4_door_passenger_volume;
-alter table all_vehicles_model_public drop column annual_fuel_cost_for_fuel_type1;
+# alter table all_vehicles_model_public drop column annual_fuel_cost_for_fuel_type1;        -- UI 단에서 사용하는 컬럼으로, drop 대상에서 제외.
 alter table all_vehicles_model_public drop column annual_fuel_cost_for_fuel_type2;
 alter table all_vehicles_model_public drop column annual_petroleum_consumption_for_fuel_type1;
 alter table all_vehicles_model_public drop column annual_petroleum_consumption_for_fuel_type2;
